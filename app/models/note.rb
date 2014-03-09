cf43cf8a14fd99813b3aec0ab784b3238b4b57bf
Florian Meißner <developer@mystler.eu>
2014-03-09 20:18:52 +0100

    Remove unnecessary callbacks when updating notes

--- app/models/note.rb
+++ app/models/note.rb
@@ -4,18 +4,16 @@ class Note < ActiveRecord::Base
 
   belongs_to :user
 
-  before_save :generate_permalink
+  before_create :generate_permalink
 
   def to_param
     self.permalink
   end
 
   def generate_permalink
-    unless self.permalink
-      begin
-        link = SecureRandom.urlsafe_base64
-      end while Note.exists?(permalink: link)
-      self.permalink = link
-    end
+    begin
+      link = SecureRandom.urlsafe_base64
+    end while Note.exists?(permalink: link)
+    self.permalink = link
   end
 end
