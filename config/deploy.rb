48e778b2cb54f1bce9e5ee189479fe6d92cfe85e
Florian Meißner <developer@mystler.eu>
2017-05-10 13:03:55 +0200

    More capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,3 +28,6 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/syst
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+# Restart passenger with tmp/restart.txt
+set :passenger_restart_with_touch, true
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
-server 'mystler.eu', user: 'mystler'
+server 'mystler.eu', user: 'mystler', roles: %w{web app db}
 set :rails_env, 'production'
 set :deploy_to, '/home/mystler/apps/Grummle'
