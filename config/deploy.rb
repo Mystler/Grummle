7e4b323aaeb9dc3d34f4a6274fc9088f5ce8459c
Florian Meißner <developer@mystler.eu>
2019-03-11 20:00:49 +0100

    Update Capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
-lock '3.10.2'
+lock "3.10.2"
 
-set :application, 'Grummle'
-set :repo_url, 'https://github.com/Mystler/Grummle.git'
+set :application, "Grummle"
+set :repo_url, "https://github.com/Mystler/Grummle.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -18,10 +18,10 @@ set :repo_url, 'https://github.com/Mystler/Grummle.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: '/opt/ruby/bin:$PATH' }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
-server 'mystler.eu', user: 'mystler', roles: %w{web app db}
-set :rails_env, 'production'
-set :deploy_to, '/home/mystler/apps/Grummle'
+server "mystler.eu", user: "mystler", roles: %w{web app db}
+set :rvm_custom_path, "/usr/share/rvm"
+set :rails_env, "production"
+set :deploy_to, "/home/mystler/apps/Grummle"
